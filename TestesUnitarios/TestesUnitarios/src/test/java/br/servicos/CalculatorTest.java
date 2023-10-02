bc44c66df9a71bb3cc406630fd9dce4ab9b1f03b
Rafabosse <35574176+Rafabosse@users.noreply.github.com>
2023-10-02 20:17:38 -0300

    Add files via upload

---
+++ TestesUnitarios/TestesUnitarios/src/test/java/br/servicos/CalculatorTest.java
@@ -0,0 +1,20 @@
+package br.servicos;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@Test
+	public void testAdd() {
+
+		//cenario
+		CalculatorTest calculator = new CalculatorTest();
+
+		//acao
+		  
+		//verificacao
+		
+	}
+}
